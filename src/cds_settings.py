7f6b91e8ef41d17ec5062b4fe6a87686daa40f24
Oleg Yakovenko <oyakovenko@roox.ru>
2012-10-26 20:15:25 +0400

    comment added

--- src/cds_settings.py
+++ src/cds_settings.py
@@ -9,5 +9,8 @@ BIG_SHOW_SIMPLATE_TIMEOUT_SECONDS = 15
 SIMPLE_ID_BIG_SHOW = "big_show"
 SIMPLE_ID_FEEDBACK = "happy_metter"
 
+#Uncomment the True setting to add new clients with the isVip flag set to 1.
+#Restarting of the server is needed
+
 #MAKE_VIP_CLIENTS = True
 MAKE_VIP_CLIENTS = False 
